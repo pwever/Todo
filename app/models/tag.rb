20be31506898d8e92cc4e3cd28a07fd1f38aec99
Pascal Wever <pwever@knittingpixel.com>
2011-07-25 17:54:02 -0700

    Added tag.setColor method to be called before saving a tag to the database.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,10 @@
 class Tag < ActiveRecord::Base
   has_and_belongs_to_many :todos
+  before_save :setColor
+  
+  def setColor
+    sha1 = Digest::SHA1.hexdigest(self.label)
+    self.color = "#"+sha1.match("[0-9a-f]{6}")[0]
+  end
+  
 end
