4678f984fae0b9592e1a276c86217e5164297b15
Pascal Wever <pwever@knittingpixel.com>
2011-08-14 20:23:39 -0700

    Enabled daily reminders via cron job.

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -1,13 +1,16 @@
 class ReminderMailer < ActionMailer::Base
   default :from => "lavell@knittingpixel.com"
   default :to => "pwever@knittingpixel.com"
+  
+  # Trigger with cron. For example:
+  # 00 07 * * * cd /WebApps/Todo; /var/lib/gems/1.8/bin/rails runner "ReminderMailer.deliver_daily"
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.reminder_mailer.daily_reminder.subject
   #
-  def daily_reminder
+  def daily
     @todos = Todo.where(:done => false).select {|todo| todo.due_today? }
     
     mail
